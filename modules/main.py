abb441e298efa4a26e31d51c941cfe040e37a5b1
S. J. Lee <occidere@naver.com>
2018-08-15 23:49:34 +0900

    main.py 위치 main 패키지 내부로 이동

--- modules/main.py
+++
@@ -1,6 +0,0 @@
-import modules.parser as parser
-import json
-
-json_result = parser.get_json_result(top20_list=parser.get_top20_list())
-json_string_result = json.loads(json_result)
-print(json_string_result)
